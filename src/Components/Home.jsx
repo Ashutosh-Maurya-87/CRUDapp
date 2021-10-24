301f5bde8c32a331cb38e409da431130f3de92b8
Ashutosh-Maurya-87 <ashumaurya486@gmail.com>
2021-10-24 18:54:02 +0530

    'building-app'

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,28 +2,47 @@ import axios from "axios";
 import React, { useState, useEffect } from "react";
 
 const Home = () => {
-  const [allData, setallData] = useState("");
-
+  const [showResult, setshowResult] = useState("initial result");
+  const [users, updateUsers] = useState({
+    page: 1,
+    data: [],
+    total_page: 1,
+    total: 1,
+    per_page: 6,
+  });
   useEffect(() => {
-    findApiData();
-  }, []);
-
-  const findApiData = () => {
     axios
-      .get("https://reqres.in/api/users?page=2")
-      .then((res) => {
-        const response = res.data.email;
-        console.log(response);
+      .get(`https://reqres.in/api/users?page=2`)
+      .then((data) => {
+        updateUsers({ ...data.data });
       })
-      .catch((error) => {
-        console.error(`Error: ${error}`);
+      .catch((err) => {
+        console.log(`error`, err);
       });
+  }, []);
+  const createUser = () => {
+    axios
+      .post(`https://reqres.in/api/users?page=2`, {
+        name: "ashutosh maurya",
+        job: "react developer",
+      })
+      .then(() => {});
   };
 
   return (
     <div>
       <h3>welcome to the home component</h3>
-      <button onClick={findApiData}>get api data</button>
+      <button>get api data</button>
+      {users.data &&
+        users.data.map((user) => (
+          <div>
+            <p>
+              {user.id}
+              {user.first_name}
+            </p>
+          </div>
+        ))}
+      <button onClick={createUser}>create user</button>
     </div>
   );
 };
